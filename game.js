02f4948a26d636003324f74d8b5a0074e6167682
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-26 22:32:49 +0100

    added basic paddle

--- game.js
+++ game.js
@@ -17,4 +17,41 @@ function randUpTo(num, floor = false) {
   return floor ? Math.floor(res) : res;
 }
 
+class Paddle {
+  constructor() {
+    const { w, h } = settings.paddleSize;
+    this.x = canvas.width / 2 - w / 2;
+    this.y = canvas.height - h * 3;
+    this.w = w;
+    this.h = h;
+  }
+
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
+}
+
 const [canvas, ctx] = new2dCanvas("play-area", 800, 500);
+
+const settings = {
+  paddleSize: {
+    w: 50,
+    h: 20,
+  },
+};
+
+const state = {
+  paddle: new Paddle(),
+  over: false,
+};
+
+function handlePaddle() {
+  state.paddle.draw();
+}
+
+(function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handlePaddle();
+  if (!state.over) requestAnimationFrame(animate);
+})();
