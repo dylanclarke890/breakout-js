9b0cff2059d2cda079f3bab7d5e3605952e6ea0f
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-26 22:42:07 +0100

    added basic controls for paddle

--- game.js
+++ game.js
@@ -26,6 +26,15 @@ class Paddle {
     this.h = h;
   }
 
+  update() {
+    const { left, right } = state.movement;
+    // no movement or moving in both directions at once.
+    if ((!left && !right) || (right && left)) return;
+    this.x += left ? settings.movementSpeed * -1 : settings.movementSpeed;
+    if (this.x < 0) this.x = 0;
+    if (this.x + this.w > canvas.width) this.x = canvas.width - this.w;
+  }
+
   draw() {
     ctx.fillStyle = "white";
     ctx.fillRect(this.x, this.y, this.w, this.h);
@@ -36,17 +45,51 @@ const [canvas, ctx] = new2dCanvas("play-area", 800, 500);
 
 const settings = {
   paddleSize: {
-    w: 50,
+    w: 80,
     h: 20,
   },
+  movementSpeed: 10,
 };
 
 const state = {
   paddle: new Paddle(),
   over: false,
+  movement: {
+    left: false,
+    right: false,
+  },
 };
 
+window.addEventListener("keydown", (e) => {
+  switch (e.key.toLowerCase()) {
+    case "arrowleft":
+    case "a":
+      state.movement = { ...state.movement, left: true };
+      break;
+    case "arrowright":
+    case "d":
+      state.movement = { ...state.movement, right: true };
+    default:
+      break;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  switch (e.key.toLowerCase()) {
+    case "arrowleft":
+    case "a":
+      state.movement = { ...state.movement, left: false };
+      break;
+    case "arrowright":
+    case "d":
+      state.movement = { ...state.movement, right: false };
+    default:
+      break;
+  }
+});
+
 function handlePaddle() {
+  state.paddle.update();
   state.paddle.draw();
 }
 
