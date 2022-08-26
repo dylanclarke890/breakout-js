370fd073e071c6b86ebb125a4b6ca8d864ae907a
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-26 22:23:11 +0100

    added js file

---
+++ game.js
@@ -0,0 +1,20 @@
+function new2dCanvas(id, width, height) {
+  const canvas = document.getElementById(id);
+  const ctx = canvas.getContext("2d");
+  canvas.width = width;
+  canvas.height = height;
+  return [canvas, ctx];
+}
+
+function drawText(text, font, fillStyle, x, y, maxWidth = undefined) {
+  if (font) ctx.font = font;
+  if (fillStyle) ctx.fillStyle = fillStyle;
+  ctx.fillText(text, x, y, maxWidth);
+}
+
+function randUpTo(num, floor = false) {
+  const res = Math.random() * num;
+  return floor ? Math.floor(res) : res;
+}
+
+const [canvas, ctx] = new2dCanvas("play-area", 800, 500);
