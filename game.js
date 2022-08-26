93777eabbacf808f126bfb4eff7c369b47b29e87
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-27 00:26:54 +0100

    added ball with collision

--- game.js
+++ game.js
@@ -17,6 +17,33 @@ function randUpTo(num, floor = false) {
   return floor ? Math.floor(res) : res;
 }
 
+function isCircleRectColliding(circle, rect) {
+  const distX = Math.abs(circle.x - rect.x - rect.w / 2);
+  const distY = Math.abs(circle.y - rect.y - rect.h / 2);
+  if (distX > rect.w / 2 + circle.r) return false;
+  if (distY > rect.h / 2 + circle.r) return false;
+  if (distX <= rect.w / 2) return true;
+  if (distY <= rect.h / 2) return true;
+  const dx = distX - rect.w / 2;
+  const dy = distY - rect.h / 2;
+  return dx * dx + dy * dy <= circle.r * circle.r;
+}
+
+function isRectRectColliding(first, second) {
+  if (!first || !second) return false;
+  if (
+    !(
+      first.x > second.x + second.width ||
+      first.x + first.width < second.x ||
+      first.y > second.y + second.height ||
+      first.y + first.height < second.y
+    )
+  ) {
+    return true;
+  }
+  return false;
+}
+
 class Paddle {
   constructor() {
     const { w, h } = settings.paddle;
@@ -27,6 +54,7 @@ class Paddle {
   }
 
   update() {
+    if (!state.started) return;
     const { left, right } = state.movement;
     // no movement or moving in both directions at once.
     if ((!left && !right) || (right && left)) return;
@@ -57,7 +85,92 @@ class Brick {
   }
 }
 
+class Ball {
+  constructor(trajectory) {
+    this.x = canvas.width / 2;
+    this.y = canvas.height - 80;
+    this.r = 10;
+    this.speed = 3;
+    this.startingSpeed = this.speed;
+    this.maxSpeed = 12;
+    this.collisionCount = 0;
+    this.trajectory = trajectory;
+  }
+
+  update() {
+    if (!state.started) return;
+    const xMovement =
+      this.trajectory.x === DIRECTION.LEFT ? this.speed : -this.speed;
+    const yMovement =
+      this.trajectory.y === DIRECTION.UP ? this.speed : -this.speed;
+    this.x = Math.floor(this.x - xMovement);
+    this.y = Math.floor(this.y - yMovement);
+    let hasCollided = false;
+
+    if (this.x - this.r <= 0) {
+      hasCollided = true;
+      this.trajectory.x = DIRECTION.RIGHT;
+    }
+    if (this.x + this.r >= canvas.width) {
+      hasCollided = true;
+      this.trajectory.x = DIRECTION.LEFT;
+    }
+    if (this.y - this.r <= 0) {
+      hasCollided = true;
+      this.trajectory.y = DIRECTION.DOWN;
+    }
+    if (isCircleRectColliding(this, state.paddle)) {
+      hasCollided = true;
+      this.trajectory.y = DIRECTION.UP;
+    }
+
+    const destroyedBricks = [];
+    state.bricks.forEach((brick, i) => {
+      if (!isCircleRectColliding(this, brick)) return;
+      if (this.x - this.r <= brick.x) {
+        hasCollided = true;
+        this.trajectory.x = DIRECTION.RIGHT;
+      }
+      if (this.x + this.r >= brick.x + brick.w) {
+        hasCollided = true;
+        this.trajectory.x = DIRECTION.LEFT;
+      }
+      if (this.y - this.r <= brick.y) {
+        hasCollided = true;
+        this.trajectory.y = DIRECTION.UP;
+      }
+      if (this.y - this.r <= brick.y + brick.h) {
+        hasCollided = true;
+        this.trajectory.y = DIRECTION.DOWN;
+      }
+      destroyedBricks.push(brick);
+    });
+    state.bricks = state.bricks.filter((b) => !destroyedBricks.includes(b));
+
+    // Periodically increase the speed based off of the amount of collisions.
+    if (hasCollided) this.collisionCount++;
+    if (this.collisionCount > 0 && this.collisionCount % 5 === 0) {
+      this.speed = Math.min(this.speed + 1, this.maxSpeed);
+      this.collisionCount = 0; // otherwise will infinitely speed up once it first hits 5.
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
+    ctx.fill();
+  }
+}
+
 const [canvas, ctx] = new2dCanvas("play-area", 800, 500);
+
+const DIRECTION = {
+  UP: "U",
+  DOWN: "D",
+  LEFT: "L",
+  RIGHT: "R",
+};
 
 const settings = {
   bricks: {
@@ -83,6 +196,8 @@ const levels = [
 
 const state = {
   paddle: new Paddle(),
+  ball: new Ball({ x: "L", y: "U" }),
+  started: false,
   over: false,
   movement: {
     left: false,
@@ -101,6 +216,8 @@ window.addEventListener("keydown", (e) => {
     case "arrowright":
     case "d":
       state.movement = { ...state.movement, right: true };
+    case " ":
+      state.started = true;
     default:
       break;
   }
@@ -136,6 +253,11 @@ function handlePaddle() {
   state.paddle.draw();
 }
 
+function handleBall() {
+  state.ball.update();
+  state.ball.draw();
+}
+
 function handleBricks() {
   for (let i = 0; i < state.bricks.length; i++) {
     state.bricks[i].update();
@@ -146,6 +268,7 @@ function handleBricks() {
 (function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   handlePaddle();
+  handleBall();
   handleBricks();
   if (!state.over) requestAnimationFrame(animate);
 })();
