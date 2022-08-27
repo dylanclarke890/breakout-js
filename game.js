4d881be8e582944999f195f23cd5b040141af10a
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-27 12:20:24 +0100

    moved levels to separate file

--- game.js
+++ game.js
@@ -187,23 +187,6 @@ const settings = {
   movementSpeed: 10,
 };
 
-const levels = [
-  [
-    [0, 1, true],
-    [1, 2, true],
-    [5, 8, true],
-    [12, 3, true],
-    [15, 14, true],
-  ],
-  [
-    [4, 1, true],
-    [3, 2, true],
-    [6, 3, true],
-    [13, 3, true],
-    [1, 14, true],
-  ],
-];
-
 const state = {
   paddle: new Paddle(),
   ball: new Ball({ x: "L", y: "U" }),
---
+++ levels.js
@@ -0,0 +1,16 @@
+const levels = [
+  [
+    [0, 1, true],
+    [1, 2, true],
+    [5, 8, true],
+    [12, 3, true],
+    [15, 14, true],
+  ],
+  [
+    [4, 1, true],
+    [3, 2, true],
+    [6, 3, true],
+    [13, 3, true],
+    [1, 14, true],
+  ],
+];
